63d6fcd573ebd05b29aaf8926ae8632d7ea4f5fb
Greger Hälltorp <greger.halltorp@gmail.com>
2016-02-09 23:29:16 +0100

    composition

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -1,22 +1,27 @@
 "use strict";
 
 var React = require("react");
-//var AuthorApi = require("../../api/authorApi");
+var AuthorApi = require("../../api/authorApi");
+var AuthorList = require("./authorList");
 
 var Authors = React.createClass({
+  getInitialState: function () {
+    return {
+      authors: []
+    };
+  },
+  componentDidMount: function () {
+    if (this.isMounted()) {
+      this.setState({
+        authors: AuthorApi.getAllAuthors()
+      });
+    }
+  },
   render: function () {
     return (
       <div>
         <h1>Authors</h1>
-        <table className="table">
-          <thead>
-          <th>ID</th>
-          <th>Name</th>
-          </thead>
-          <tbody>
-          {this.state.author.map}
-          </tbody>
-        </table>
+        <AuthorList authors={this.state.authors} />
       </div>
     );
   }
